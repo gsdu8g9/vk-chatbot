6d698f0a91d256f6ce94873f8725c238a5ed4d00
Nikita Obrekht <obrekht@gmail.com>
2016-11-27 18:32:44 +0200

    'Update'

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import requests
 import random
 import vk_requests as vk
 from vk_requests.exceptions import VkAPIError
+from enum import IntEnum, unique
 
 CHAT_OFFSET = 2000000000
 
@@ -18,12 +19,17 @@ def remove_urls(text):
     return re.sub(r'(\S+\.\S{2,20})+', '[url_removed]', text, 0, 0)
 
 
+message_flags = {'unread': 1, 'outbox': 2, 'replied': 4, 'important': 8, 'chat': 16, 'friends': 32, 'spam': 64,
+                'deleted': 128, 'fixed': 256, 'media': 512}
+
+
 class Bot(object):
     def __init__(self):
 
         self.start_time = None
         self.api = None
         self.bot_user = None
+        self.long_poll_server = None
         self.lang = 'ru'
 
         try:
@@ -32,6 +38,8 @@ class Bot(object):
             self.version = self.config['DEFAULT']['Version']
             self.vk_api_version = self.config['DEFAULT']['VkApiVersion']
             self.admin_id = self.config['DEFAULT']['AdminId']
+
+            self.poll_config = {'mode': 170, 'wait': 25, 'version': 1}
         except configparser.Error:
             log.exception('Error while reading config')
         except KeyError:
@@ -40,6 +48,9 @@ class Bot(object):
         log.info('Initializing...')
 
     def start(self):
+        if self.is_started():
+            return False
+
         self.start_time = datetime.datetime.now()
 
         try:
@@ -49,30 +60,25 @@ class Bot(object):
                                      api_version=self.vk_api_version, interactive=True, lang=self.lang)
 
             self.bot_user = self.api.users.get()[0]
+            self.long_poll_server = self.api.messages.getLongPollServer(use_ssl=1)
         except VkAPIError as e:
             log.exception('Connection failure: {}'.format(e.message))
             return False
 
-        log.info('Authorization success.\n  Name: {} {}, ID: {}.'.format(
-            self.bot_user['first_name'], self.bot_user['last_name'], self.bot_user['id'])
+        log.info('Authorization success.')
+        log.info('Name: {} {}, ID: {}, lang: {}'.format(
+            self.bot_user['first_name'], self.bot_user['last_name'], self.bot_user['id'], self.lang)
         )
 
         log.info('Connected!')
 
         # start long polling
-        poll_config = {'mode': 170, 'wait': 25, 'version': 1}
-
         try:
-            long_poll = self.api.messages.getLongPollServer(use_ssl=1)
-            poll_server = 'https://{server}?act=a_check&key={key}&ts={ts}&wait={wait}&mode={mode}\
-            &version={version}'.format(
-                server=long_poll['server'], key=long_poll['key'], ts=long_poll['ts'],
-                wait=poll_config['wait'], mode=poll_config['mode'], version=poll_config['version'])
+            request_url = self.get_long_poll_server_url(self.long_poll_server['ts'])
 
             while True:
-
                 try:
-                    response = requests.post(poll_server).json()
+                    response = requests.post(request_url).json()
                 except ValueError:
                     response = None
                     pass
@@ -80,10 +86,7 @@ class Bot(object):
                 log.debug('Response: {}'.format(response))
 
                 if response:
-                    poll_server = 'https://{server}?act=a_check&key={key}&ts={ts}&wait={wait}&mode={mode}\
-                    &version={version}'.format(
-                        server=long_poll['server'], key=long_poll['key'], ts=response['ts'],
-                        wait=poll_config['wait'], mode=poll_config['mode'], version=poll_config['version'])
+                    request_url = self.get_long_poll_server_url(response['ts'])
 
                     for update in response['updates']:
 
@@ -99,6 +102,7 @@ class Bot(object):
             return True
         except VkAPIError as e:
             log.error('%s %s', e.message, e.error_data)
+            return False
 
     def _handle_message(self, message):
         log.debug('Message: {}'.format(message))
@@ -109,21 +113,22 @@ class Bot(object):
         timestamp = message[4]
         text = message[6]
 
+        chat_id = None
+
         user_id = None
         user = None
 
-        is_chat = False
-
         if peer_id > CHAT_OFFSET:
+            chat_id = peer_id - CHAT_OFFSET
             user_id = message[7].get("from", "?")
             user = self.api.users.get(user_ids=user_id)[0]
-            title = message[5]
+            chat_title = message[5]
             is_chat = True
         else:
             user_id = str(message[3])
             user = self.api.users.get(user_ids=user_id)[0]
 
-        if flags & 2 == 0: # if inbox
+        if flags & message_flags['outbox'] == 0:  # if inbox
             if text == "!s":
                 if self._is_admin(user_id):
                     if 'PRODUCTION' in os.environ:
@@ -138,7 +143,7 @@ class Bot(object):
                     self.api.messages.send(peer_id=peer_id, message=s_text)
                 else:
                     self.api.messages.send(peer_id=peer_id, message='\U0001F643')  # upside down face emoji
-            elif re.match(r'при+ве+т(?:[\s,]+)(?:ма+рку+с|ma+rcu+s)', text, flags=re.IGNORECASE) if is_chat else \
+            elif re.match(r'при+ве+т(?:[\s,]+)(?:ма+рку+с|ma+rcu+s)', text, flags=re.IGNORECASE) if chat_id else \
                     re.match(r'при+ве+т(?:\W+|$)', text, flags=re.IGNORECASE):
                 emojies = ['\U0001F60E', '\U0001F60A', '\U0001F603', '\U0001F609']
                 self.api.messages.send(peer_id=peer_id,
@@ -148,6 +153,16 @@ class Bot(object):
 
     def _is_admin(self, user_id):
         return user_id == self.admin_id
+
+
+    def get_long_poll_server_url(self, ts_):
+        return 'https://{server}?act=a_check&key={key}&ts={ts}&wait={wait}&mode={mode}\
+                    &version={version}'.format(
+            server=self.long_poll_server['server'], key=self.long_poll_server['key'], ts=ts_,
+            wait=self.poll_config['wait'], mode=self.poll_config['mode'], version=self.poll_config['version'])
+
+    def is_started(self):
+        return self.start_time != None
 
 
 if __name__ == '__main__':
