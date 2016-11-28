0553a11d3885209998c20e726764493f562833e5
Nikita Obrekht <obrekht@gmail.com>
2016-11-28 02:09:27 +0200

    Big refactoring

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
 import configparser
-import datetime
 import logging
 import os
 import re
 import requests
-import random
+from datetime import datetime
 import vk_requests as vk
 from vk_requests.exceptions import VkAPIError
-from enum import IntEnum, unique
 
 CHAT_OFFSET = 2000000000
 
@@ -19,8 +17,12 @@ def remove_urls(text):
     return re.sub(r'(\S+\.\S{2,20})+', '[url_removed]', text, 0, 0)
 
 
+events = {'set_flags': 1, 'update_flags': 2, 'reset_flags': 3, 'add_message': 4, 'read_inbox': 6, 'read_outbox': 7,
+          'online': 8, 'offline': 9, 'update_chat_params': 51, 'start_typing': 61, 'start_typing_char': 62,
+          'update_unread_count': 80, 'update_notification_params': 114}
+
 message_flags = {'unread': 1, 'outbox': 2, 'replied': 4, 'important': 8, 'chat': 16, 'friends': 32, 'spam': 64,
-                'deleted': 128, 'fixed': 256, 'media': 512}
+                 'deleted': 128, 'fixed': 256, 'media': 512}
 
 
 class Bot(object):
@@ -51,7 +53,7 @@ class Bot(object):
         if self.is_started():
             return False
 
-        self.start_time = datetime.datetime.now()
+        self.start_time = datetime.now()
 
         try:
             self.api = vk.create_api(app_id=os.environ['VK_APP_ID'], login=os.environ['VK_LOGIN'],
@@ -61,6 +63,8 @@ class Bot(object):
 
             self.bot_user = self.api.users.get()[0]
             self.long_poll_server = self.api.messages.getLongPollServer(use_ssl=1)
+
+            self._add_commands()
         except VkAPIError as e:
             log.exception('Connection failure: {}'.format(e.message))
             return False
@@ -74,28 +78,7 @@ class Bot(object):
 
         # start long polling
         try:
-            request_url = self.get_long_poll_server_url(self.long_poll_server['ts'])
-
-            while True:
-                try:
-                    response = requests.post(request_url).json()
-                except ValueError:
-                    response = None
-                    pass
-
-                log.debug('Response: {}'.format(response))
-
-                if response:
-                    request_url = self.get_long_poll_server_url(response['ts'])
-
-                    for update in response['updates']:
-
-                        if update[0] == 4:
-                            try:
-                                self._handle_message(update)
-                            except VkAPIError as e:
-                                log.error('%s\n%s', e.message, e.error_data)
-                    pass
+            self._start_long_polling()
 
         except KeyboardInterrupt:
             log.info('Interrupt received, shutting down...')
@@ -104,56 +87,69 @@ class Bot(object):
             log.error('%s %s', e.message, e.error_data)
             return False
 
-    def _handle_message(self, message):
-        log.debug('Message: {}'.format(message))
+    def _start_long_polling(self):
+        request_url = self.get_long_poll_server_url(self.long_poll_server['ts'])
 
-        message_id = message[1]
-        flags = message[2]
-        peer_id = message[3]
-        timestamp = message[4]
-        text = message[6]
+        while True:
+            try:
+                response = requests.post(request_url).json()
+            except ValueError:
+                response = None
+                pass
 
-        chat_id = None
+            log.debug('Response: {}'.format(response))
 
-        user_id = None
-        user = None
+            if response:
+                request_url = self.get_long_poll_server_url(response['ts'])
 
-        if peer_id > CHAT_OFFSET:
-            chat_id = peer_id - CHAT_OFFSET
-            user_id = message[7].get("from", "?")
-            user = self.api.users.get(user_ids=user_id)[0]
-            chat_title = message[5]
-            is_chat = True
+                for update in response['updates']:
+                    try:
+                        self._handle_event(update)
+                    except VkAPIError as e:
+                        log.error('%s\n%s', e.message, e.error_data)
+                pass
+
+    def _handle_event(self, event):
+        log.debug('Event: {}'.format(event))
+
+        if event[0] == events['add_message']:
+            self._handle_message(message_id=event[1], flags=event[2], peer_id=event[3], timestamp=event[4],
+                                 chat_title=event[5], text=event[6], attachments=event[7], random_id=event[8])
+
+    def _handle_message(self, **fields):
+
+        if fields['peer_id'] > CHAT_OFFSET:
+            fields['is_chat'] = True
+            fields['chat_id'] = fields['peer_id'] - CHAT_OFFSET
+            fields['user_id'] = fields['attachments'].get('from')
+            fields['source_act'] = fields['attachments'].get('source_act')
+            fields['source_mid'] = fields['attachments'].get('source_mid')
         else:
-            user_id = str(message[3])
-            user = self.api.users.get(user_ids=user_id)[0]
+            fields['is_chat'] = False
+            fields['user_id'] = str(fields['peer_id'])
+            fields['source_act'] = None
+            fields['source_mid'] = None
 
-        if flags & message_flags['outbox'] == 0:  # if inbox
-            if text == "!s":
-                if self._is_admin(user_id):
-                    if 'PRODUCTION' in os.environ:
-                        server = 'production'
+        fields['user'] = self.api.users.get(user_ids=fields['user_id'])[0]
+
+        for command in self.commands:
+            if command.event == events['add_message']:
+                text = command.fields.get('text')
+                flags = command.fields.get('flags')
+                lack_flags = command.fields.get('lack_flags')
+                source_act = command.fields.get('source_act')
+                source_mid = command.fields.get('source_mid')
+
+                if (text is None or text == fields['text']) and (flags is None or fields['flags'] & flags == flags) \
+                        and (lack_flags is None or fields['flags'] & lack_flags == fields['flags']) \
+                        and (source_act is None or source_act == fields['source_act']) \
+                        and (source_mid is None or str(source_mid) == str(fields['source_mid'])):
+                    if command.function(self, **fields):
+                        return True
                     else:
-                        server = 'test'
+                        pass
 
-                    now_time = datetime.datetime.now()
-                    s_text = 'Bot v{}\nUptime: {}\nRunning on: {}'.format(
-                        self.version, now_time - self.start_time, server
-                    )
-                    self.api.messages.send(peer_id=peer_id, message=s_text)
-                else:
-                    self.api.messages.send(peer_id=peer_id, message='\U0001F643')  # upside down face emoji
-            elif re.match(r'при+ве+т(?:[\s,]+)(?:ма+рку+с|ma+rcu+s)', text, flags=re.IGNORECASE) if chat_id else \
-                    re.match(r'при+ве+т(?:\W+|$)', text, flags=re.IGNORECASE):
-                emojies = ['\U0001F60E', '\U0001F60A', '\U0001F603', '\U0001F609']
-                self.api.messages.send(peer_id=peer_id,
-                                       message='Привет, {}! {}{}'.format(
-                                           user['first_name'], random.choice(emojies), random.choice(emojies))
-                                       )
-
-    def _is_admin(self, user_id):
-        return user_id == self.admin_id
-
+        return False
 
     def get_long_poll_server_url(self, ts_):
         return 'https://{server}?act=a_check&key={key}&ts={ts}&wait={wait}&mode={mode}\
@@ -162,7 +158,24 @@ class Bot(object):
             wait=self.poll_config['wait'], mode=self.poll_config['mode'], version=self.poll_config['version'])
 
     def is_started(self):
-        return self.start_time != None
+        return self.start_time is not None
+
+    def _add_commands(self):
+        import commands
+
+        self.commands = [
+            Command(events['add_message'], commands.status, text='!s', lack_flags=(~message_flags['outbox'])),
+            Command(events['add_message'], commands.hello, lack_flags=(~message_flags['outbox'])),
+            Command(events['add_message'], commands.invite, source_act='chat_invite_user',
+                    source_mid=self.bot_user['id'], lack_flags=(~message_flags['outbox']))
+        ]
+
+
+class Command(object):
+    def __init__(self, event, function, **fields):
+        self.event = event
+        self.function = function
+        self.fields = fields
 
 
 if __name__ == '__main__':
---
+++ commands.py
@@ -0,0 +1,44 @@
+import os
+import random
+import re
+import logging
+from datetime import datetime
+
+log = logging.getLogger('vk-bot')
+
+
+def status(bot, **fields):
+    if fields['user_id'] == bot.admin_id:
+        if 'PRODUCTION' in os.environ:
+            server = 'production'
+        else:
+            server = 'test'
+
+        now_time = datetime.now()
+        formatted_text = 'Bot v{}\nUptime: {}\nRunning on: {}'.format(
+            bot.version, now_time - bot.start_time, server
+        )
+        bot.api.messages.send(peer_id=fields['peer_id'], message=formatted_text)
+    else:
+        bot.api.messages.send(peer_id=fields['peer_id'], message='\U0001F643')  # upside down face emoji
+
+    return True
+
+
+def hello(bot, **fields):
+    if re.match(r'при+ве+т(?:[\s,]+)(?:ма+рку+с|ma+rcu+s)', fields['text'], flags=re.IGNORECASE) \
+            if fields['is_chat'] else re.match(r'при+ве+т(?:\W+|$)', fields['text'], flags=re.IGNORECASE):
+        emojies = ['\U0001F60E', '\U0001F60A', '\U0001F603', '\U0001F609']
+        bot.api.messages.send(peer_id=fields['peer_id'],
+                              message='Привет, {}! {}{}'.format(
+                                  fields['user']['first_name'], random.choice(emojies), random.choice(emojies))
+                              )
+        return True
+
+    return False
+
+
+def invite(bot, **fields):
+    bot.api.messages.send(peer_id=fields['peer_id'],
+                          message='Привет всем!')
+    return True
